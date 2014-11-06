9453c74d410fea6516de4b43345efddb0aa05fff
Douglas Bailey <DBailey01@gmail.com>
2014-11-05 21:41:32 -0600

    Kevin's ride........
    Part 2 done???????

--- ScenegraphWithLightsTextures/ScenegraphWithLightsTextures/Scenegraph.cpp
+++ ScenegraphWithLightsTextures/ScenegraphWithLightsTextures/Scenegraph.cpp
@@ -27,6 +27,9 @@ void Scenegraph::makeScenegraph(Node *r)
 	chair1 = static_cast<TransformNode*>(root->getNode("chair1"));
 	chair2 = static_cast<TransformNode*>(root->getNode("chair2"));
 	chair3 = static_cast<TransformNode*>(root->getNode("chair3"));
+
+
+
 }
 
 
@@ -172,27 +175,43 @@ void Scenegraph::draw(stack<glm::mat4>& modelView)
 
 void Scenegraph::animate(float time)
 {
-	time = time*2;
+	time = time*8;
 	if(cabin != NULL) {
-		//cabin->setAnimationTransform(glm::rotate(glm::mat4(1.0),time*0.4f,glm::vec3(0,1,0)));
+               cabin->setAnimationTransform(glm::rotate(glm::mat4(1.0),cos(time),glm::vec3(0,0,1)) * glm::translate(glm::mat4(1.0),glm::vec3(0,-75,0)));
+       }
 
-		//cabin->setAnimationTransform(glm::rotate(glm::mat4(1.0),cos(time),glm::vec3(0,0,1)) * glm::translate(glm::mat4(1.0),glm::vec3(0,-75,0)));
-		//cabin->setAnimationTransform(glm::translate(glm::mat4(1.0),glm::vec3(cos(time*5)*50,0,0)) * glm::translate(glm::mat4(1.0),glm::vec3(0,-75,0)));
+       if(chair0 != NULL) {
+               chair0->setAnimationTransform(glm::rotate(glm::mat4(1.0),time,glm::vec3(0,1,0)));
+               chair1->setAnimationTransform(glm::translate(glm::mat4(1.0),glm::vec3(cos(time*5)*10,0,0)));
+               chair2->setAnimationTransform(glm::rotate(glm::mat4(1.0),time,glm::vec3(0,1,0)));
+               chair3->setAnimationTransform(glm::rotate(glm::mat4(1.0),time,glm::vec3(0,1,0)));
+       }
+
+	TransformNode *	pole1 = static_cast<TransformNode*>(root->getNode("pole1"));
+	if(pole1!=NULL){
+		pole1->setAnimationTransform(glm::rotate(glm::mat4(1.0f),time*8.0f,glm::vec3(0,1,0)));
 	}
 
-	if(chair1 != NULL) {
-		//chair1->setAnimationTransform(glm::rotate(glm::mat4(1.0),time,glm::vec3(0,1,0)) * chair1->getTransform());
-		//chair1->setAnimationTransform(glm::rotate(glm::mat4(1.0),cos(time),glm::vec3(0,1,0)) * chair1->getTransform());
-
-		
-		chair0->setAnimationTransform(glm::rotate(glm::mat4(1.0),time,glm::vec3(0,1,0)));
-		chair1->setAnimationTransform(glm::translate(glm::mat4(1.0),glm::vec3(cos(time*5)*10,0,0)));
-		//chair1->setAnimationTransform(glm::rotate(glm::mat4(1.0),time,glm::vec3(0,1,0)));
-		//chair2->setAnimationTransform(glm::rotate(glm::mat4(1.0),time,glm::vec3(0,1,0)));
-		//chair3->setAnimationTransform(glm::rotate(glm::mat4(1.0),time,glm::vec3(0,1,0)));
-		
+	
+	TransformNode *	pole2 = static_cast<TransformNode*>(root->getNode("pole2"));
+	if(pole2!=NULL){
+		pole2->setAnimationTransform(glm::rotate(glm::mat4(1.0f),time*-8.0f,glm::vec3(0,1,0)));
 	}
 
+	TransformNode *	pole3 = static_cast<TransformNode*>(root->getNode("pole3"));
+	if(pole3!=NULL){
+		pole3->setAnimationTransform(glm::rotate(glm::mat4(1.0f),time*8.0f,glm::vec3(0,1,0)));
+	}
+
+	TransformNode *	pole4 = static_cast<TransformNode*>(root->getNode("pole4"));
+	if(pole4!=NULL){
+		pole4->setAnimationTransform(glm::rotate(glm::mat4(1.0f),time*-8.0f,glm::vec3(0,1,0)));
+	}
+	
+	TransformNode *	spinner = static_cast<TransformNode*>(root->getNode("spinner"));
+	if(spinner!=NULL){
+		spinner->setAnimationTransform(glm::rotate(glm::mat4(1.0f),time*8,glm::vec3(0,1,0)));
+	}
 }
 
 void Scenegraph::getLights(stack<glm::mat4>& modelView){
